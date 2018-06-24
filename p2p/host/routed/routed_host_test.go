bb7ee9bbe2a9a2e298c1da6b63d4c8ad2bb77ed3
Jessie Chatham Spencer <jessie@teainspace.com>
2018-06-24 22:25:14 +0200

    Add testcase for a routed host to have no addresses
    Tests that a routed host will use the router to search
    for addresses if no known addresses were present in the
    peerinfo supplied in the Connect function or in the routed
    host's own peerstore.

--- p2p/host/routed/routed_host_test.go
+++ p2p/host/routed/routed_host_test.go
@@ -88,3 +88,40 @@ func TestConnectWithOldAddrs(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+// TestConnectWithNoAddrs tests that a routed host uses the router
+// to search for addresses if the suppplied peerinfo and the routed
+// host's own peerstore do not contain any addresses for the
+// peer that it is attempting a connection with.
+func TestConnectWithNoAddrs(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	host1, err := libp2p.New(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	host2, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/15001"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testRouter := TestRouter{make(map[peer.ID]peerstore.PeerInfo)}
+	routedHost1 := Wrap(host1, testRouter)
+
+	// Attempt to connect where routedhost1 does not have the peerinfo for host2
+	// and the router doesn't either, so the connection should fail.
+	if err := routedHost1.Connect(ctx, peerstore.PeerInfo{ID: host2.ID()}); err == nil {
+		t.Fatal(errors.New("The routedhost should not be able to connect at this stage."))
+	}
+
+	// Add host2's peerinfo to the router.
+	testRouter.insertPeerInfo(host2.ID(), peerstore.PeerInfo{ID: host2.ID(), Addrs: host2.Addrs()})
+
+	// Attempt to connect again, this time the router has the peerinfo for host2
+	// so the connection should succeed.
+	if err := routedHost1.Connect(ctx, host1.Peerstore().PeerInfo(host2.ID())); err != nil {
+		t.Fatal(err)
+	}
+}
