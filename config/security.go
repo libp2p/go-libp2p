09249652c4d6c2a0924b6ed53ff11055088af4e7
Raúl Kripalani <raul@protocol.ai>
2019-08-18 11:05:56 +0200

    reorder imports.

--- config/security.go
+++ config/security.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"fmt"
-	"github.com/libp2p/go-libp2p-core/crypto"
 
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/sec"
