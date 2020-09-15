33340b1c3e8fb74531b3b90a37c06265c7f0e192
Alex Towle <jalextowle@gmail.com>
2020-09-15 09:55:32 -0500

    Added filtering for "InterceptSecured"

--- options_filter.go
+++ options_filter.go
@@ -27,8 +27,8 @@ func (f *filtersConnectionGater) InterceptAccept(connAddr network.ConnMultiaddrs
 	return !(*ma.Filters)(f).AddrBlocked(connAddr.RemoteMultiaddr())
 }
 
-func (f *filtersConnectionGater) InterceptSecured(_ network.Direction, _ peer.ID, _ network.ConnMultiaddrs) (allow bool) {
-	return true
+func (f *filtersConnectionGater) InterceptSecured(_ network.Direction, _ peer.ID, connAddr network.ConnMultiaddrs) (allow bool) {
+	return !(*ma.Filters)(f).AddrBlocked(connAddr.RemoteMultiaddr())
 }
 
 func (f *filtersConnectionGater) InterceptUpgraded(_ network.Conn) (allow bool, reason control.DisconnectReason) {
