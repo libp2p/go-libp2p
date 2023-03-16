56119e9299b67078d471935c9a8b2771144fa6bb
Chinmay Kousik <chinmaykousik1@gmail.com>
2023-03-16 20:14:43 +0530

    rename protofile

--- p2p/transport/webrtc_w3c/proto.go
+++ p2p/transport/webrtc_w3c/proto.go
@@ -1,3 +1,3 @@
 package webrtc_w3c
 
-//go:generate protoc --proto_path=$PWD:$PWD/../../.. --go_out=. --go_opt=Mpb/message.proto=./pb pb/message.proto
+//go:generate protoc --proto_path=$PWD:$PWD/../../.. --go_out=. --go_opt=Mpb/webrtc.proto=./pb pb/webrtc.proto
