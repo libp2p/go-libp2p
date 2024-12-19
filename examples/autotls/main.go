3d9290cd62d91bf844ddd494d6041f237dd275b6
Daniel N <2color@users.noreply.github.com>
2024-12-19 16:59:58 +0100

    fix: improve example and reuse same port

--- examples/autotls/main.go
+++ examples/autotls/main.go
@@ -9,10 +9,9 @@ import (
 
 	"github.com/ipfs/go-log/v2"
 
-	"github.com/libp2p/go-libp2p"
-	kad "github.com/libp2p/go-libp2p-kad-dht"
-
 	p2pforge "github.com/ipshipyard/p2p-forge/client"
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	quic "github.com/libp2p/go-libp2p/p2p/transport/quic"
 	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
 	ws "github.com/libp2p/go-libp2p/p2p/transport/websocket"
@@ -25,17 +24,17 @@ func main() {
 	ctx := context.Background()
 
 	log.SetLogLevel("*", "error")
-	log.SetLogLevel("basichost", "info") // Set the log level for the basichost package to info
-	log.SetLogLevel("autotls", "debug")  // Set the log level for the autotls-example package to debug
-	log.SetLogLevel("p2pforge", "debug") // Set the log level for the p2pforge package to debug
-	log.SetLogLevel("nat", "debug")      // Set the log level for the p2pforge package to debug
+	log.SetLogLevel("basichost", "info")  // Set the log level for the basichost package to info
+	log.SetLogLevel("autotls", "debug")   // Set the log level for the autotls-example package to debug
+	log.SetLogLevel("p2p-forge", "debug") // Set the log level for the p2pforge package to debug
+	log.SetLogLevel("nat", "debug")       // Set the log level for the p2pforge package to debug
 
 	certLoaded := make(chan bool, 1) // Create a channel to signal when the cert is loaded
 
 	// p2pforge is the AutoTLS client library.
 	// The cert manager handles the creation and management of certificate
 	certManager, err := p2pforge.NewP2PForgeCertMgr(
-		p2pforge.WithCAEndpoint(p2pforge.DefaultCATestEndpoint), // Use the Let's Encrypt staging CA to avoid rate limiting
+		p2pforge.WithCAEndpoint(p2pforge.DefaultCAEndpoint), // Let's Encrypt production CA. You can also use the staging CA (p2pforge.DefaultCATestEndpoint) to avoid rate limiting.
 		p2pforge.WithOnCertLoaded(func() {
 			certLoaded <- true
 		}),
@@ -55,14 +54,13 @@ func main() {
 		libp2p.NATPortMap(),   // Attempt to open ports using UPnP for NATed hosts.
 
 		libp2p.ListenAddrStrings(
-			"/ip4/0.0.0.0/tcp/15000",         // regular tcp connections
-			"/ip4/0.0.0.0/udp/15000/quic-v1", // a UDP endpoint for the QUIC transport
+			"/ip4/0.0.0.0/tcp/5500",         // regular tcp connections
+			"/ip4/0.0.0.0/udp/5500/quic-v1", // a UDP endpoint for the QUIC transport
 
 			// AutoTLS will automatically generate a certificate for this host
 			// and use the forge domain (`libp2p.direct`) as the SNI hostname.
-			// This must use a separate port from the regular tcp/udp ports.
-			fmt.Sprintf("/ip4/0.0.0.0/tcp/15001/tls/sni/*.%s/ws", p2pforge.DefaultForgeDomain),
-			fmt.Sprintf("/ip6/::/tcp/15001/tls/sni/*.%s/ws", p2pforge.DefaultForgeDomain),
+			fmt.Sprintf("/ip4/0.0.0.0/tcp/5500/tls/sni/*.%s/ws", p2pforge.DefaultForgeDomain),
+			fmt.Sprintf("/ip6/::/tcp/5500/tls/sni/*.%s/ws", p2pforge.DefaultForgeDomain),
 		),
 
 		// Configure the TCP transport
@@ -74,6 +72,7 @@ func main() {
 		// Configure the WS transport with the AutoTLS cert manager
 		libp2p.Transport(ws.New, ws.WithTLSConfig(certManager.TLSConfig())),
 
+		libp2p.UserAgent("go-libp2p/example/autotls"),
 		// AddrsFactory takes the multiaddrs we're listening on and sets the multiaddrs to advertise to the network.
 		// We use the AutoTLS address factory so that the `*` in the AutoTLS address string is replaced with the
 		// actual IP address of the host once detected
@@ -86,12 +85,12 @@ func main() {
 
 	logger.Info("Host created: ", h.ID())
 
-	// TODO: Add a comment about why we need the DHT
-	dhtOpts := []kad.Option{
-		kad.Mode(kad.ModeAuto),
-		kad.BootstrapPeers(kad.GetDefaultBootstrapPeerAddrInfos()...),
+	// Bootstrap the DHT to verify our public IPs address with AutoNAT
+	dhtOpts := []dht.Option{
+		dht.Mode(dht.ModeClient),
+		dht.BootstrapPeers(dht.GetDefaultBootstrapPeerAddrInfos()...),
 	}
-	dht, err := kad.New(ctx, h, dhtOpts...)
+	dht, err := dht.New(ctx, h, dhtOpts...)
 	if err != nil {
 		panic(err)
 	}
@@ -100,7 +99,6 @@ func main() {
 
 	time.Sleep(5 * time.Second)
 
-	logger.Info("RT size: ", dht.RoutingTable().Size())
 	logger.Info("Addresses: ", h.Addrs())
 
 	certManager.ProvideHost(h)
